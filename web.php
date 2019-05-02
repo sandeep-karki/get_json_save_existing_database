2238ab22147ebe330284d151e7f7a7cac7818d94
sandeep karki <37303792+sandeep-karki@users.noreply.github.com>
2019-05-02 16:41:05 +0545

    Update web.php

--- web.php
+++ web.php
@@ -5,19 +5,11 @@ use App\Resturant;
 
 Route::get('/', function () {
 	$resturantList= Resturant::get();
-	// dd($resturantList);
 	foreach ($resturantList as $list) {
-		// dd($list['openinghours']);
-		// dd($list['latitude']);
-		// dd($list);
 		$path = public_path() . "/resturants.json";
 		$json = file_get_contents($path);
 		$decoded=json_decode($json,true);
-		// dd($decoded);
 		foreach ($decoded["vendors"] as $vendor) {
-			// dd($vendor['OpeningHours']);
-			// dd($vendor);
-			// dd($vendor['LocationLat']);
 			if($vendor['LocationLat'] == $list['latitude']){
 				// $vendor['OpeningHours'] = $list['openinghours'];
 				$list['openinghours'] = $vendor['OpeningHours'];
