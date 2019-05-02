310e90ca3011ba90a202e4813ce59a3f81e0ccf4
sandeep karki <37303792+sandeep-karki@users.noreply.github.com>
2019-05-02 16:44:52 +0545

    Update web.php

--- web.php
+++ web.php
@@ -4,14 +4,17 @@ use App\Resturant;
 
 
 Route::get('/', function () {
+	//get data from model
 	$resturantList= Resturant::get();
 	foreach ($resturantList as $list) {
+		//get json data
 		$path = public_path() . "/resturants.json";
 		$json = file_get_contents($path);
 		$decoded=json_decode($json,true);
 		foreach ($decoded["vendors"] as $vendor) {
+			//matching column data
 			if($vendor['LocationLat'] == $list['latitude']){
-				// $vendor['OpeningHours'] = $list['openinghours'];
+				//updating the column data
 				$list['openinghours'] = $vendor['OpeningHours'];
 			}
 			$list->save();
